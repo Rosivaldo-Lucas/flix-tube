f27e8f2d6816eb884d0e4cbbf134ad711606fa95
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-03-05 20:46:10 -0300

    ms-video-encoder: add video download, persist, fragment and encode

--- ms-video-encoder/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_encoder/service/VideoProcessingManager.java
+++ ms-video-encoder/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_encoder/service/VideoProcessingManager.java
@@ -1,5 +1,6 @@
 package br.com.rosivaldolucas.flixtube.ms_video_encoder.service;
 
+import br.com.rosivaldolucas.flixtube.ms_video_encoder.cloud.dto.DownloadResponseDTO;
 import br.com.rosivaldolucas.flixtube.ms_video_encoder.entity.Video;
 import br.com.rosivaldolucas.flixtube.ms_video_encoder.messaging.dto.VideoUploadedEventDTO;
 import br.com.rosivaldolucas.flixtube.ms_video_encoder.repository.VideoRepository;
@@ -8,18 +9,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class VideoProcessingManager {
 
+    @Value("${ms-video-encoder.tmp-dir}")
+    private String TMP_DIR;
+
     @Value("${ms-video-encoder.bucket}")
     private String BUCKET;
 
-    @Value("${ms-video-encoder.input-file-path}")
+    @Value("${ms-video-encoder.input-path}")
     private String INPUT_FILE_PATH;
 
-    @Value("${ms-video-encoder.output-file-path}")
+    @Value("${ms-video-encoder.output-path}")
     private String OUTPUT_FILE_PATH;
 
     private final VideoRepository videoRepository;
@@ -34,34 +44,49 @@ public class VideoProcessingManager {
         this.videoRepository.save(video);
 
         try {
-            this.download(video);
+            String pathDir = String.format("%s/%s", this.TMP_DIR, video.getId());
+            String filePath = String.format("%s/%s", pathDir, video.getId());
+            String key = String.format("%s/%s", video.getInputFilePath(), video.getInputFilename());
 
-            // TODO: armazenar o video localmente
+            this.updateStatus(video, "DOWNLOADING");
+            DownloadResponseDTO downloadResponseDTO = this.videoProcessingService.download(this.BUCKET, key);
 
-            this.fragment(video);
+            this.persistInLocal(pathDir, video.getId(), downloadResponseDTO.contentAsInputStream());
 
-            this.encode(video);
+            this.updateStatus(video, "FRAGMENTING");
+            this.videoProcessingService.fragment(filePath, filePath);
 
-            // TODO: upload do video encodado
+            this.updateStatus(video, "ENCODING");
+            this.videoProcessingService.encode(filePath, filePath);
+
+            this.updateStatus(video, "UPLOADING");
+            this.videoProcessingService.upload(filePath, video.getOutputFilePath());
         } catch (Exception ex) {
-            video.updateStatus("FAILED");
-            this.videoRepository.save(video);
+            this.updateStatus(video, "FAILED");
         }
     }
 
-    private void download(Video video) {
-        video.updateStatus("DOWNLOADING");
-        this.videoProcessingService.downloadVideo(video);
+    private void updateStatus(Video video, String status) {
+        video.updateStatus(status);
+        this.videoRepository.save(video);
     }
 
-    private void fragment(Video video) {
-        video.updateStatus("FRAGMENTING");
-        this.videoProcessingService.fragmentVideo(video);
-    }
+    private void persistInLocal(String pathDir, String filename, InputStream content) {
+        Path path = Path.of(pathDir);
 
-    private void encode(Video video) {
-        video.updateStatus("ENCODING");
-        this.videoProcessingService.encodeVideo(video);
+        try {
+            Files.createDirectories(path);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+
+        Path filePath = path.resolve(filename);
+
+        try (OutputStream os = Files.newOutputStream(filePath)) {
+            os.write(content.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- ms-video-encoder/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_encoder/service/VideoProcessingService.java
+++ ms-video-encoder/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_encoder/service/VideoProcessingService.java
@@ -1,23 +1,71 @@
 package br.com.rosivaldolucas.flixtube.ms_video_encoder.service;
 
-import br.com.rosivaldolucas.flixtube.ms_video_encoder.entity.Video;
+import br.com.rosivaldolucas.flixtube.ms_video_encoder.cloud.S3Integration;
+import br.com.rosivaldolucas.flixtube.ms_video_encoder.cloud.dto.DownloadResponseDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class VideoProcessingService {
 
-    public void downloadVideo(Video video) {
-        log.info("video downloaded");
+    private final S3Integration s3Integration;
+
+    public DownloadResponseDTO download(String bucket, String key) {
+        return this.s3Integration.downloadFile(bucket, key);
     }
 
-    public void fragmentVideo(Video video) {
-        log.info("video fragmented");
+    public void fragment(String inputFilePath, String outputFilePath) {
+        String inputPathWithExtension = String.format("%s.mp4", inputFilePath);
+        String outputPathWithExtension = String.format("%s.frag", outputFilePath);
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("mp4fragment", inputPathWithExtension, outputPathWithExtension);
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException();
+            }
+        } catch (IOException | InterruptedException ex) {
+            throw new RuntimeException();
+        }
     }
 
-    public void encodeVideo(Video video) {
-        log.info("video encoded");
+    public void encode(String inputFilePath, String outputDir) {
+        String inputPathWithExtension = String.format("%s.frag", inputFilePath);
+
+        String[] args = {
+                inputPathWithExtension,
+                "--use-segment-timeline",
+                "-o", outputDir,
+                "-f",
+                "--exec-dir", "/opt/bento4/bin/"
+        };
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("mp4dash", args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException();
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void upload(String inputFilePath, String outputFilePath) {
+        String inputPathWithExtension = String.format("%s.mpd", inputFilePath);
     }
 
 }
