70df3e96924bcbf6a6a800a2ae8d90bd0a8a6058
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-03-05 12:24:37 -0300

    ms-video-encoder: add initial commit

---
+++ ms-video-encoder/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_encoder/MsVideoEncoderApplication.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.flixtube.ms_video_encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVideoEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVideoEncoderApplication.class, args);
+	}
+
+}
