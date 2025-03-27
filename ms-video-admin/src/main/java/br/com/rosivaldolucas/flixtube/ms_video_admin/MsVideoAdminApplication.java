20f5ac802642260ad0bd2ea91075239af9fd1fdd
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-03-26 21:19:06 -0300

    ms-video-admin: add initial project

---
+++ ms-video-admin/src/main/java/br/com/rosivaldolucas/flixtube/ms_video_admin/MsVideoAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.rosivaldolucas.flixtube.ms_video_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVideoAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsVideoAdminApplication.class, args);
+	}
+
+}
